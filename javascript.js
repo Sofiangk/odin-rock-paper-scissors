6080ab751b261f61138ad1af8a2feb89202254e9
sofiangk <sofian.a.r.gargouri@gmail.com>
2023-03-26 21:52:53 +0200

    Add playRound function

--- javascript.js
+++ javascript.js
@@ -1,5 +1,43 @@
 let option = ["Rock", "Paper", "Scissors"];
 
 function getComputerChoice() {
-  return option[Math.floor(Math.random() * option.length)];
+    return option[Math.floor(Math.random() * option.length)];
 }
+
+function playRound(playerSelection) {
+  switch ((playerSelection, computerSelection)) {
+    case playerSelection === "Rock":
+      if (computerSelection === "Scissors") {
+        return "You win! Rock beats Scissors!";
+      } else if (computerSelection === "Paper") {
+        return "You Lose! Paper beats Rock!";
+      } else {
+        return "It's a tie!";
+      }
+      break;
+
+    case playerSelection === "Paper":
+      if (computerSelection === "Scissors") {
+        return "You Lose! Scissors beats Paper!";
+      } else if (computerSelection === "Rock") {
+        return "You Win! Paper beats Rock!";
+      } else {
+        return "It's a tie!";
+      }
+      break;
+
+    case playerSelection === "Scissors":
+      if (computerSelection === "Rock") {
+        return "You Lose! Rock beats Scissors!";
+      } else if (computerSelection === "Paper") {
+        return "You Win! Scissors beats Paper!";
+      } else {
+        return "It's a tie!";
+      }
+      break;
+
+    default:
+      return "Wrong entry!!";
+  }
+}
+
