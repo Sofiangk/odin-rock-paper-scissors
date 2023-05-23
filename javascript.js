f924f01b84e42b9ea7f87aa05e25a638c09f3a97
sofiangk <sofian.a.r.gargouri@gmail.com>
2023-05-23 11:31:54 +0200

    Update game ui and logic

--- javascript.js
+++ javascript.js
@@ -1,69 +1,114 @@
-let option = ["Rock", "Paper", "Scissors"];
+// Define variable and DOM objects
+let option = ["rock", "paper", "scissors"];
 
-let computerSelection = "";
-let computerScore = 0;
-let playerSelection = "";
+let cpuScore = 0;
 let playerScore = 0;
+let playerSelection, cpuSelection;
 
-const playerText = document.querySelector("#player-text");
-const computerText = document.querySelector("#computer-text");
+const gameContainer = document.querySelector("#game-container");
 const resultText = document.querySelector("#result-text");
-const choiceBtn = document.querySelectorAll(".choiceBtn");
 const playerScoreText = document.querySelector("#player-score");
-const computerScoreText = document.querySelector("#computer-score");
+const cpuScoreText = document.querySelector("#cpu-score");
+const playerChoice = document.querySelector("#player-choice img");
+const cpuChoice = document.querySelector("#cpu-choice img");
+const choiceSelection = document.querySelectorAll(".option-image");
 
-choiceBtn.forEach((button) =>
-  button.addEventListener("click", () => {
-    playerSelection = button.id;
-    computerSelection = getComputerChoice();
-    playerText.textContent = `Player: ${playerSelection}`;
-    computerText.textContent = `Computer: ${computerSelection}`;
-    resultText.textContent = playRound();
-    playerScoreText.textContent = `Player Score: ${playerScore}`;
-    computerScoreText.textContent = `Computer Score: ${computerScore}`;
-    if (playerScore === 5) {
-      alert("YOU WIN!!!");
-      playerScore = 0;
-      computerScore = 0;
-    }
-    if (computerScore === 5) {
-      alert("GAME OVER!! YOU LOSE!!!");
-      playerScore = 0;
-      computerScore = 0;
-    }
-  })
-);
+// Loop each option image
+choiceSelection.forEach((image, index) => {
+  image.addEventListener("click", (e) => {
+    image.classList.add("active");
+
+    cpuChoice.src = playerChoice.src = "/images/rock.png";
+    resultText.textContent = "Wait...";
+
+    // Loop each option image again
+    choiceSelection.forEach((image2, index2) => {
+      // Remove "active" class from other options
+      // If current index doesn't match clicked index
+      if (index !== index2) image2.classList.remove("active");
+    });
+
+    // Assign player and cpu choices
+    playerSelection = image.id;
+    cpuSelection = getComputerChoice();
+
+    // Start game with choice selection
+    gameContainer.classList.add("start");
+    // playerScoreText.classList.remove("point");
+    // cpuScoreText.classList.remove("point");
+
+    // Set a timeout to delay the result calculation
+    let time = setTimeout(() => {
+      gameContainer.classList.remove("start");
+      playerChoice.src = `/images/${playerSelection}.png`;
+      cpuChoice.src = `/images/${cpuSelection}.png`;
+      resultText.textContent = playRound();
+      playerScoreText.textContent = `Player: ${playerScore}`;
+      cpuScoreText.textContent = `CPU: ${cpuScore}`;
+
+      // Add animation to score if player/cpu won
+      if (resultText === "You Win!!") {
+        playerScoreText.classList.add("point");
+      } else if (resultText === "You Lose!!") {
+        cpuScoreText.classList.add("point");
+      }
+
+      if (playerScore === 5) {
+        // Ending game after a player reaches 5 wins
+        resultText.textContent = "YOU ARE THE WINNER!!!";
+        playerScore = 0;
+        cpuScore = 0;
+        gameContainer.classList.add("over");
+      }
+      if (cpuScore === 5) {
+        resultText.textContent = "GAME OVER!! YOU LOSE!!!";
+        playerScore = 0;
+        cpuScore = 0;
+        gameContainer.classList.add("over");
+      }
+    }, 2000);
+    gameContainer.classList.remove("over");
+  });
+});
+
+// choiceSelection.forEach((image) =>
+//   image.addEventListener("click", () => {
+//     playerChoice.src = `/image/${playerSelection.toLowerCase()}.png`;
+//     cpuChoice.src = `/image/${cpuChoice.toLowerCase()}.png`;
+//     resultText.innerHTML = playRound();
+//   })
+// );
 
 function getComputerChoice() {
-  return option[Math.floor(Math.random() * option.length)];
+  return option[Math.floor(Math.random() * 3)];
 }
 
 function playRound() {
-  if (playerSelection === computerSelection) {
-    return "It's a tie!";
-  } else if (computerSelection === "Rock") {
-    if (playerSelection === "Paper") {
+  if (playerSelection === cpuSelection) {
+    return "It's a draw!";
+  } else if (cpuSelection === "rock") {
+    if (playerSelection === "paper") {
       playerScore++;
-      return "You Win! Paper beats Rock!";
+      return "You Win!!";
     } else {
-      computerScore++;
-      return "You Lose! Rock beats Scissors!";
+      cpuScore++;
+      return "You Lose!!";
     }
-  } else if (computerSelection === "Paper") {
-    if (playerSelection === "Scissors") {
+  } else if (cpuSelection === "paper") {
+    if (playerSelection === "scissors") {
       playerScore++;
-      return "You Win! Scissors beats Paper!";
+      return "You Win!!";
     } else {
-      computerScore++;
-      return "You Lose! Paper beats Rock!";
+      cpuScore++;
+      return "You Lose!!";
     }
-  } else if (computerSelection === "Scissors") {
-    if (playerSelection === "Rock") {
+  } else if (cpuSelection === "scissors") {
+    if (playerSelection === "rock") {
       playerScore++;
-      return "You Win! Rock beats Scissors!";
+      return "You Win!!";
     } else {
-      computerScore++;
-      return "You Lose! Scissors beats Paper!";
+      cpuScore++;
+      return "You Lose!!";
     }
   }
 }
